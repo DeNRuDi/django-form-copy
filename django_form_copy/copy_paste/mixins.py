95b295f9cf6731657f7050b0847c9ee72b9e477b
denis <dl.devop.wwl@gmail.com>
2024-01-22 18:07:29 +0200

    Added: version with image fields copy_paste available

--- django_form_copy/copy_paste/mixins.py
+++ django_form_copy/copy_paste/mixins.py
@@ -1,6 +1,12 @@
+from django.db.models.fields.files import ImageFieldFile, ImageField
 from django.db.models import ForeignKey, ManyToManyField
+
+from django.core.files.storage import FileSystemStorage
+from django.core.files.base import ContentFile
+
 from django.forms import model_to_dict
 from django.contrib import messages
+from django.conf import settings
 from django.apps import apps
 
 from dateutil.parser import parse, ParserError
@@ -16,7 +22,7 @@ class UtilsMixin:
     def _is_datetime(datetime_string: str) -> datetime | bool:
         try:
             return parse(datetime_string)
-        except ParserError:
+        except (ParserError, OverflowError):
             return False
 
     def _creating_actual_info_from_cb_process(self, model_info: dict) -> dict:
@@ -27,9 +33,9 @@ class UtilsMixin:
                 # foreign key
                 model = models_dict.get(values['model'])
                 if model:
+                    values = self._pre_format_model_dict(values, to_datetime=True)
                     values.pop('model', None)
                     values.pop('id', None)
-                    values = self._pre_format_model_dict(values, to_datetime=True)
                     obj, _ = model.objects.get_or_create(**values)
                     model_info.update({field: obj.id})
             elif isinstance(values, list):
@@ -38,16 +44,23 @@ class UtilsMixin:
                 for m2m_item in values:
                     model = models_dict.get(m2m_item['model'])
                     if model:
-                        m2m_item.pop('model', None)
-                        m2m_item.pop('id', None)
                         m2m_item = self._pre_format_model_dict(m2m_item, to_datetime=True)
+                        m2m_item.pop('id', None)
+                        m2m_item.pop('model', None)
                         obj, created = model.objects.get_or_create(**m2m_item)
                         m2m_ids.append(obj.id)
                 model_info.update({field: m2m_ids})
             elif isinstance(values, str):
                 result = self._is_datetime(values)
                 model_info.update({field: result}) if result else None
+            elif isinstance(values, tuple):
+                name, image_bytes, model_name = values
+                image = ImageField(storage=FileSystemStorage(location=settings.BASE_DIR))
+                image.attname = model_name
 
+                content_file = ContentFile(image_bytes, name=name)
+                image_file_field = ImageFieldFile(instance=content_file, field=image, name=name)
+                model_info.update({field: image_file_field}) if image_file_field else None
         return model_info
 
     def _pre_format_model_dict(self, obj_dict: dict, to_datetime: bool = False) -> dict:
@@ -60,6 +73,13 @@ class UtilsMixin:
             elif isinstance(value, str) and to_datetime:
                 result = self._is_datetime(value)
                 value = result if result else value
+            elif isinstance(value, ImageFieldFile):
+                if value and value.file:
+                    with value.open('rb') as file:
+                        file_bytes = file.read()
+                    value = (value.name, file_bytes, obj_dict['model']) if file_bytes else None
+                else:
+                    value = None
             field_info.update({key: value})
 
         return field_info
@@ -69,8 +89,8 @@ class UtilsMixin:
         m2m_exclude.append('id')
 
         obj_dict = model_to_dict(obj, exclude=m2m_exclude)
-        obj_dict = self._pre_format_model_dict(obj_dict)
         obj_dict['model'] = obj.__class__.__name__
+        obj_dict = self._pre_format_model_dict(obj_dict)
 
         for field in obj._meta.fields:
             if isinstance(field, ForeignKey):
@@ -79,7 +99,6 @@ class UtilsMixin:
                     related_obj_dict = self._expand_model_relations(related_obj)
                     obj_info = self._pre_format_model_dict(related_obj_dict)
                     obj_dict.update({field.name: obj_info})
-
         for field in obj._meta.many_to_many:
             if isinstance(field, ManyToManyField):
                 m2m_field = getattr(obj, field.name).all()
@@ -104,6 +123,7 @@ class CopyPasteMixin(UtilsMixin):
                 copy_paste_from_clipboard = ast.literal_eval(clipboard.paste())
                 new_copy_paste_from_clipboard = self._creating_actual_info_from_cb_process(copy_paste_from_clipboard)
                 initial_data.update(**new_copy_paste_from_clipboard)
+                request.session['initial_data'] = clipboard.paste()
                 messages.success(request, 'Data pasted successfully.')
             except Exception as err:
                 messages.error(request, f'Failed to paste - data in the clipboard is not valid. | {err}')
@@ -118,3 +138,23 @@ class CopyPasteMixin(UtilsMixin):
             extra_context = extra_context or {}
             extra_context['info'] = info
         return super().change_view(request, object_id, form_url, extra_context)
+
+    def response_post_save_add(self, request, obj):
+        data = request.POST
+        initial_data_str = request.session.get('initial_data')
+        if initial_data_str:
+            initial_data = ast.literal_eval(initial_data_str)
+            initial_data = self._creating_actual_info_from_cb_process(initial_data)
+
+            for field, value in initial_data.items():
+                if isinstance(value, ImageFieldFile):
+                    image_info = data.get(field)
+                    clear_checkbox = data.get(f"{field}-clear")
+                    if clear_checkbox != 'on' and image_info == '':
+                        setattr(obj, field, value)
+
+            obj.save()
+            request.session.pop('initial_data', None)
+
+        return super().response_post_save_add(request, obj)
+
